4c2e3727a7766e6859b1d56216332d10b72656f6
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-07-30 18:18:37 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,61 @@
+# # This program prints Hello, world!
+
+# print('Hello, world!')
+
+# num1 = 1.5 
+# num2 = 1.5
+# print(num1 + num2)
+
+# a = bool(0)
+# print(a)
+
+# a = complex(1, 2)
+# print(a)
+
+# import math
+
+# radius = float(input("Enter radius of circle: "))
+
+# area = math.pi * radius ** 2
+# print(area)
+
+# name = input("Enter your name: ")
+# print("Hello " + name + "!")
+
+# a = 10
+# print(type(a))
+
+# b = 10.0
+# print(type(b))
+
+# c = True
+# print(type(c))
+
+# d = "10"
+# print(type(d))
+
+# e = "10.0"
+# print(type(e))
+
+# f = 3 + 4j
+# print(type(f))
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# sum = num1 + num2
+
+# print("Sum of {0} and {1} is {2}".format(num1, num2, sum))
+
+x= 'Hellow world'
+
+print(x)
+print(x.upper())
+print(x.lower())
+print(x[0:5])
+print(x.replace("world", "universe"))
+y = ' universe'
+
+print(x+y)
+z = 3
+print(x*z)
