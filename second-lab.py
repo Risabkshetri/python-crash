b76713db6b7d9b170ef4d530ca16f10695ab094e
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-08-20 19:20:07 +0530

    second lab end here

--- second-lab.py
+++ second-lab.py
@@ -45,3 +45,26 @@ print('h' not in a);
 
 # in
 print('h' in a)
+
+
+## write a program to enter the days and print in the format yr:month:week:day
+
+day = int(input("Enter the day: "))
+
+year = day // 365
+day = day % 365
+month = day // 30
+day = day % 30
+week = day // 7
+day = day % 7
+
+print(f"{year}:{month}:{week}:{day}")
+
+## write a program to covert the numbers of days into measures of year week and days
+
+num = int(input("Eneter any number : "));
+if(num%2 == 0):
+    print("number is even")
+
+else:
+    print("number is odd ")
