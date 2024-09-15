a919a03b08a213be7f0a0312e37ef81edd88feef
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-09-15 21:33:41 +0530

    preparing for examination

---
+++ TuT/exam.py
@@ -0,0 +1,13 @@
+# demonstrate the various method of list
+
+list1 = ["rishab", 1, 2, 5, 6, 4, 3]
+
+print(list1)
+list1.reverse()
+print(list1)
+
+list1.append("kshetri")
+print(list1)
+
+list1.insert(1,3)
+print(list1)
