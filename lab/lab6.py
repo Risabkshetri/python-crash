7e3b282813f8e1e13e606c23a6970cab66a07b5d
risabkshetri <risawgc@gmail.com>
2024-10-08 11:01:28 +0530

    lab6 end here

---
+++ lab/lab6.py
@@ -0,0 +1,15 @@
+# write python function for returning a list, tuple, dictionary.
+def createList():
+    list = [1,3,4,5,"rishab ksehtri"]
+    return list
+print("list = ",  createList())
+
+def createTuble():
+    tuple = (1,2,3,4,5)
+    return tuple
+print("Tuple = ",  createTuble())
+
+def createDict():
+    dict = {1:'Rishab Kshetri', 2: 'kasif sheikh', 3: 'Manmohan'}
+    return dict
+print("Dict = ",  createDict())
