5b75ea6af728b61c47da96a389b13a1d9a16eb92
risabkshetri <risawgc@gmail.com>
2024-10-22 10:48:50 +0530

    lab8 end here

--- lab/lab8.py
+++ lab/lab8.py
@@ -3,18 +3,18 @@
 # # b) Parameterized constructor
 # # c) area() to compute area of rectangle
 
-# class Rectangle:
-#     def __init__ (self, l=10, b=20):
-#         self.length = l
-#         self.breadth = b
-#         print("Area = ", self.length * self.breadth)
+class Rectangle:
+    def __init__ (self, l=10, b=20):
+        self.length = l
+        self.breadth = b
+        print("Area = ", self.length * self.breadth)
     
-#     def area(self,length, breadth):
-#         return length * breadth
+    def area(self,length, breadth):
+        return length * breadth
 
-# r1 = Rectangle()
-# r2 = Rectangle(20, 30)
-# print("Area of r1 = ", r1.area(70, 20))
+r1 = Rectangle()
+r2 = Rectangle(20, 30)
+print("Area of r1 = ", r1.area(70, 20))
 
 
 
